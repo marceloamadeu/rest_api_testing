76ee8f16150f495d2d203321618729a2846ef735
marceloamadeu <marceloandradeamadeu@gmail.com>
2021-11-06 14:26:44 -0300

    First commit

---
+++ src/main/java/com/rest/api/testing/app/App.java
@@ -0,0 +1,13 @@
+package com.rest.api.testing.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
